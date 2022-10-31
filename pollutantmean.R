174d8458728dd80dd0c811d162e1a259e8f74f6d
RomanSoftex <roman@softex.if.ua>
2022-10-31 14:16:20 +0200

    Add files

---
+++ pollutantmean.R
@@ -0,0 +1,11 @@
+pollutantmean<-function(directory, pollutant, id=1:332){
+  directory <- paste(getwd(),"/",directory,"/",sep="")
+  files.list<-list.files(directory)
+  dat<-data.frame()
+  for (i in id){
+    dat<-rbind(dat, read.csv(paste(directory,files.list[i],sep="")))
+  }
+  mean(dat[,pollutant], na.rm=TRUE)
+}
+
+pollutantmean("specdata","sulfate",1:10)
